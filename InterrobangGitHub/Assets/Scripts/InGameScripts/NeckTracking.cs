cd7fb3ad84536a12a3f9e7d1436e0ced9fe53f35
Dean Angus <deanangus1@gmail.com>
2018-11-10 14:57:37 +0000

    Zombie neck physics

---
+++ InterrobangGitHub/Assets/Scripts/InGameScripts/NeckTracking.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeckTracking : MonoBehaviour
+{
+
+    public float maxStretch = 3.0f;
+    public LineRenderer catapultLineFront;
+
+    private SpringJoint2D spring;
+
+    void Awake()
+    {
+        spring = GetComponent<SpringJoint2D>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        LineRendererSetup();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LineRendererSetup()
+    {
+        catapultLineFront.SetPosition(0, catapultLineFront.transform.position);
+    }
+}
---
+++ InterrobangGitHub/Assets/Scripts/InGameScripts/ZombieNeckTracker.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieNeckTracker : MonoBehaviour {
+
+    GameObject zombie;
+
+	// Use this for initialization
+	void Start ()
+    {
+        zombie = GameObject.FindGameObjectWithTag("zombie");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.position = zombie.transform.position;
+	}
+}
