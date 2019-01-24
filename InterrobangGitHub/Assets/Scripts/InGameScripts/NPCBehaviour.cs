aed6be1e394a394b7701d26a69bea63040de3d1c
Dean Angus <deanangus1@gmail.com>
2019-01-24 16:16:45 +0000

    NPC

--- InterrobangGitHub/Assets/Scripts/InGameScripts/NPCBehaviour.cs
+++ InterrobangGitHub/Assets/Scripts/InGameScripts/NPCBehaviour.cs
@@ -6,25 +6,28 @@ public class NPCBehaviour : MonoBehaviour
     public GameObject leftPoint;
     public GameObject rightPoint;
     public float movementSpeed;
+    public float idleTime;
 
     Animator anim;
     bool movingRight;
     Vector2 direction;
+    float idleCounter;
 
     // Use this for initialization
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
         direction.x = 1;
+        idleCounter = idleTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-
         //if moving right and it's less than the right point then move the body 
         if (movingRight)
         {
+            anim.SetBool("Walking Right", true);
             anim.SetBool("Walking Left", false);
             transform.position = new Vector2(transform.position.x + direction.x * movementSpeed, transform.position.y + direction.y * movementSpeed);
         }
@@ -32,17 +35,44 @@ public class NPCBehaviour : MonoBehaviour
         if (!movingRight)
         {
             anim.SetBool("Walking Left", true);
+            anim.SetBool("Walking Right", false);
             transform.position = new Vector2(transform.position.x - direction.x * movementSpeed, transform.position.y - direction.y * movementSpeed);
         }
 
-        //changed the body from left to right 
+        //when the body hits the right side
         if (gameObject.transform.position.x >= rightPoint.transform.position.x)
         {
-            movingRight = false;
+            anim.SetBool("Idle Right", true);
+            direction.x = 0;
+            idleCounter -= 1 * Time.deltaTime;
+
+            if (idleCounter <= 0)
+            {
+                anim.SetBool("Idle Right", false);
+                idleCounter = idleTime;
+                direction.x = 1;
+                movingRight = false;
+            }
         }
+
+        //when the body hits the left side
         if (gameObject.transform.position.x <= leftPoint.transform.position.x)
         {
-            movingRight = true;
+            anim.SetBool("Idle Left", true);
+            direction.x = 0;
+            idleCounter -= 1 * Time.deltaTime;
+
+            if (idleCounter <= 0)
+            {
+                anim.SetBool("Idle Left", false);
+                idleCounter = idleTime;
+                direction.x = 1;
+                movingRight = true;
+            }
         }
     }
+
+    
 }
+
+
