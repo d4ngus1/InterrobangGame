08c6084fed83c75be5274ccff34a69481b7342d6
Dean Angus <deanangus1@gmail.com>
2019-02-06 23:26:11 +0000

    New Character Added

---
+++ InterrobangGitHub/Assets/Scripts/InGameScripts/KeyScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyScript : MonoBehaviour
+{
+    public GameObject keyDoor;
+
+    bool keyCollected; 
+
+    void Update()
+    {
+        //when the key has been collected notify the door script to turn off its collider 
+        if(keyCollected)
+        {
+            keyDoor.GetComponent<LockedDoorScript>().keyCollected = true;
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "zombie")
+        {
+            keyCollected = true;            
+        }
+    }
+}
---
+++ InterrobangGitHub/Assets/Scripts/InGameScripts/LockedDoorScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockedDoorScript : MonoBehaviour
+{
+
+    [HideInInspector]
+    public bool keyCollected;
+
+    void Update()
+    {
+        //if the key has been collected then turn off the collider to let the zombie through 
+        if (keyCollected)
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+        else
+        {
+            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "zombie" && keyCollected == true)
+        {
+            gameObject.GetComponent<Animator>().SetTrigger("Open");
+        }
+    }
+}
