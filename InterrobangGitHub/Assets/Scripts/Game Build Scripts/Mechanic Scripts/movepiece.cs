e03742cac0841ca21522b375a68ff27e834ea9fe
AdamMcDonaldAbertay <43935257+AdamMcDonaldAbertay@users.noreply.github.com>
2019-02-07 11:36:53 +0000

    Add files via upload

---
+++ InterrobangGitHub/Assets/Scripts/Game Build Scripts/Mechanic Scripts/movepiece.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movepiece : MonoBehaviour {
+
+
+    public string pieceStatus = "idle";
+
+    public Transform edgeParticles;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (pieceStatus == "pickedup")
+        {
+            Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = objPosition;
+        }
+	}
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+     if (other.gameObject.name == gameObject.name)
+        {
+			Instantiate(edgeParticles, new Vector3(other.gameObject.transform.position.x, other.gameObject.transform.position.y, other.gameObject.transform.position.z), edgeParticles.rotation);
+            transform.position = other.gameObject.transform.position;
+            pieceStatus = "dropped";
+        }
+
+
+    }
+
+    void OnMouseDown()
+    {
+        pieceStatus = "pickedup";  
+    }
+
+    void OnMouseUp()
+    {
+        pieceStatus = "dropped"; 
+    }
+}
