d5a9ca8fcfe4859b6cab8ba233c5c13272e696f2
Dean Angus <deanangus1@gmail.com>
2019-02-20 15:59:55 +0000

    Mechanic Tutorial

---
+++ InterrobangGitHub/Assets/Scripts/Game Build Scripts/Mechanic Scripts/TutorialVideoObjectScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class TutorialVideoObjectScript : MonoBehaviour
+{
+    public VideoPlayer videoP;
+    public RawImage rawImage;
+    private bool characterNear;
+
+    // Use this for initialization
+    void Start()
+    {
+        videoP.Prepare();
+        rawImage.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checks when the video has finished playing
+        if ((ulong)videoP.frame == videoP.frameCount)
+        {
+            videoP.Stop();
+            videoP.Prepare();
+            rawImage.enabled = false;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (characterNear)
+        {
+            rawImage.enabled = true;
+            //when the user has pressed the icon start the video
+            videoP.Play();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        characterNear = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        characterNear = false;
+    }
+
+    
+}
--- InterrobangGitHub/Assets/Scripts/Game Build Scripts/Mechanic Scripts/TutorialVideoScript.cs
+++ InterrobangGitHub/Assets/Scripts/Game Build Scripts/Mechanic Scripts/TutorialVideoScript.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Video;
+using UnityEngine.UI;
 
-public class TutorialVideoScript : MonoBehaviour {
+public class TutorialVideoScript : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private VideoPlayer videoP;
+    public bool characterNear;
+    public RawImage image;
+
+    // Use this for initialization
+    void Start()
+    {
+        //image.texture = videoP.texture;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+       
+    }
 }
