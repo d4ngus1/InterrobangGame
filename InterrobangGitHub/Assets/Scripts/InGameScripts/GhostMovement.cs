50c999e1a4bdebc15cc7258c6959ec279a4533b6
d4ngus1 <36505897+d4ngus1@users.noreply.github.com>
2019-03-07 12:54:50 +0000

    Ghost float

--- InterrobangGitHub/Assets/Scripts/InGameScripts/GhostMovement.cs
+++ InterrobangGitHub/Assets/Scripts/InGameScripts/GhostMovement.cs
@@ -10,6 +10,8 @@ public class GhostMovement : MonoBehaviour
     public Vector3 cameraOffset;
     [Range(0, 10)]
     public float movementSpeed = 0.05f;
+    [Range(0, 20)]
+    public float floaty = 5;
     [Range(0, 2)]
     public float animationSpeed = 1;
     [Range(0, 2)]
@@ -29,12 +31,16 @@ public class GhostMovement : MonoBehaviour
     private SwitchingCharacters switchingCharacters;
     float move;
 
+    Vector2 floatyness;
+    Vector4 floatOfGhost;
+
     private void Start()
     {
         //sets up the animation we want for the ghost
         anim = GetComponent<Animator>();
         freeCam = GameObject.FindGameObjectWithTag("MainCamera");
         switchingCharacters = GameObject.FindObjectOfType<SwitchingCharacters>();
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -46,13 +52,161 @@ public class GhostMovement : MonoBehaviour
             {
                 //store the variable to be used in the animator to see whether 
                 //the ghost is moving to the left or to the right
-                
+
                 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 direction = (mousePosition - transform.position).normalized;
                 move = direction.x;
-                transform.position = new Vector2(transform.position.x + direction.x * movementSpeed * Time.deltaTime, transform.position.y + direction.y * movementSpeed * Time.deltaTime);
+                //transform.position = new Vector2(transform.position.x + direction.x * movementSpeed * Time.deltaTime, transform.position.y + direction.y * movementSpeed * Time.deltaTime);
+
+                //allows the ghost to have more of a floaty feel 
+                rb.velocity = direction * movementSpeed;
+                floatyness = direction * movementSpeed;
+
+                if (floatyness.x > 0 && floatyness.y > 0)
+                {
+                    //floating top right
+                    floatOfGhost = new Vector4(1, 0, 0, 0);
+                }
+                else if (floatyness.x > 0 && floatyness.y < 0)
+                {
+                    //floating bottom right
+                    floatOfGhost = new Vector4(0, 1, 0, 0);
+                }
+                else if (floatyness.x < 0 && floatyness.y < 0)
+                {
+                    //floating bottom left
+                    floatOfGhost = new Vector4(0, 0, 1, 0);
+                }
+                else if (floatyness.x < 0 && floatyness.y > 0)
+                {
+                    //floating top left
+                    floatOfGhost = new Vector4(0, 0, 0, 1);
+                }
             }
-            else move = 0;
+            else 
+            {
+                move = 0;
+
+                //top right
+                if (floatOfGhost.x > 0)
+                {
+                    if (floatyness.x > 0)
+                    {
+                        floatyness.x -= floaty * Time.deltaTime;
+                    }
+
+                    if (floatyness.y > 0)
+                    {
+                        floatyness.y -= floaty * Time.deltaTime;
+                    }
+
+                    if (floatyness.x < 0)
+                    {
+                        floatyness.x = 0;
+                    }
+
+                    if (floatyness.y < 0)
+                    {
+                        floatyness.y = 0;
+                    }
+
+                    if(floatyness.x == 0 && floatyness.y == 0)
+                    {
+                        floatOfGhost.x = 0;
+                    }
+                }
+
+                //bottom right
+                if (floatOfGhost.y > 0)
+                {
+                    if (floatyness.x > 0)
+                    {
+                        floatyness.x -= floaty * Time.deltaTime;
+                    }
+
+                    if (floatyness.y < 0)
+                    {
+                        floatyness.y += floaty * Time.deltaTime;
+                    }
+
+                    if (floatyness.x < 0)
+                    {
+                        floatyness.x = 0;
+                    }
+
+                    if (floatyness.y > 0)
+                    {
+                        floatyness.y = 0;
+                    }
+
+                    if (floatyness.x == 0 && floatyness.y == 0)
+                    {
+                        floatOfGhost.y = 0;
+                    }
+                }
+
+                //bottom left
+                if (floatOfGhost.z > 0)
+                {
+                    if (floatyness.x < 0)
+                    {
+                        floatyness.x += floaty * Time.deltaTime;
+                    }
+
+                    if (floatyness.y < 0)
+                    {
+                        floatyness.y += floaty * Time.deltaTime;
+                    }
+
+                    if (floatyness.x > 0)
+                    {
+                        floatyness.x = 0;
+                    }
+
+                    if (floatyness.y > 0)
+                    {
+                        floatyness.y = 0;
+                    }
+
+                    if (floatyness.x == 0 && floatyness.y == 0)
+                    {
+                        floatOfGhost.z = 0;
+                    }
+                }
+
+                //top left
+                if (floatOfGhost.w > 0)
+                {
+                    if (floatyness.x < 0)
+                    {
+                        floatyness.x += floaty * Time.deltaTime;
+                    }
+
+                    if (floatyness.y > 0)
+                    {
+                        floatyness.y -= floaty * Time.deltaTime;
+                    }
+
+                    if (floatyness.x > 0)
+                    {
+                        floatyness.x = 0;
+                    }
+
+                    if (floatyness.y < 0)
+                    {
+                        floatyness.y = 0;
+                    }
+
+                    if (floatyness.x == 0 && floatyness.y == 0)
+                    {
+                        floatOfGhost.w = 0;
+                    }
+                }
+
+
+
+                rb.velocity = floatyness;
+            }
 
             //sends the data from the player to the condition in the animation to allow it to change transitions 
             anim.SetFloat("gSpeed", move);
